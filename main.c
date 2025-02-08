cdc105e4bc7ecd890d0fae4a4c93ce9922c83767
RezMir1 <RezvovMilan@gmail.com>
2025-02-08 09:43:20 +0300

    Prpmpt user for last name as well

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 
 int main(int argc, char **argv)
 {
-    char name[255];
+    char name[255],last [255];
 
-    printf("Enter your name: ");
+    printf("Enter your first name: ");
     fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); /* debug line */
-    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-    printf("Hello %s!\n", name);
+    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+
+   printf("Now enter your last name: ");
+    gets(last); /* buffer overflow? what's that? */
+
+    printf("Hello %s!\n", first, last);
     return 0;
 }
