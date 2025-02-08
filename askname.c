320df7b21341c491a959f1fdf80b52c8b6baa920
RezMir1 <RezvovMilan@gmail.com>
2025-02-08 11:57:32 +0300

    Update askname.c
    change

--- askname.c
+++ askname.c
@@ -2,8 +2,8 @@
 #include <string.h>
  void askname(char *first, char *last)
 {
-
-printf("Now enter your first name: ");
+cout<<endl;
+printf("Now enter your 1st name: ");
  fgets(first, 255, stdin);
     first[strlen(first)-2] = '\0'; /* remove the newline at the end */
 	printf("Now enter your last name: ");
